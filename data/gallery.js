0db7f3c1219b7b75b916372ac98dddec1932d2b5
joechaeik <joe.chaeik@gmail.com>
2026-03-12 16:36:48 +0900

    Update gallery: 2024 7th Korea Toray Science & Technology Award

--- data/gallery.js
+++ data/gallery.js
@@ -38,7 +38,7 @@ var GALLERY = [
   {
     "src": "https://epa.kentech.ac.kr/files/attach/images/2083/119/005/5de6daa89e11722cd0f88ed6cfbfe017.jpg",
     "caption": "2024 7th Korea Toray Science & Technology Award",
-    "category": "Prize",
+    "category": "Award",
     "year": 2024
   },
   {
