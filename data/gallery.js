4ab5c2e717c00fc4e4187778e03e5ec84a1cacbe
joechaeik <joe.chaeik@gmail.com>
2026-03-12 09:14:54 +0900

    Update gallery: 240207 group photo

--- data/gallery.js
+++ data/gallery.js
@@ -8,7 +8,7 @@ var GALLERY = [
   {
     "src": "https://raw.githubusercontent.com/joechaeik/epa-homepage/main/images/gallery/1773274352755_240207_group_photo.jpg",
     "caption": "240207 group photo",
-    "category": "Group 2024",
+    "category": "Group",
     "year": 2024
   }
 ];
