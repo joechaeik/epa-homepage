d161bdec274ed3f99849d4e90b24d4b8c35319f7
joechaeik <joe.chaeik@gmail.com>
2026-03-12 09:24:29 +0900

    Add gallery: 240430 group photo

--- data/gallery.js
+++ data/gallery.js
@@ -1,14 +1,20 @@
 var GALLERY = [
   {
+    "src": "https://raw.githubusercontent.com/joechaeik/epa-homepage/main/images/gallery/lab_dinner.jpg",
+    "caption": "EPA Lab Dinner",
+    "category": "Group",
+    "year": 2025
+  },
+  {
     "src": "https://raw.githubusercontent.com/joechaeik/epa-homepage/main/images/gallery/1773274352755_240207_group_photo.jpg",
     "caption": "240207 group photo",
     "category": "Group",
     "year": 2024
   },
   {
-    "src": "https://raw.githubusercontent.com/joechaeik/epa-homepage/main/images/gallery/lab_dinner.jpg",
-    "caption": "EPA Lab Dinner",
+    "src": "https://raw.githubusercontent.com/joechaeik/epa-homepage/main/images/gallery/1773275067275_240430_group_photo.jpg",
+    "caption": "240430 group photo",
     "category": "Group",
-    "year": 2025
+    "year": 2024
   }
 ];
