07c8e08cdd3eb32e8dfb674837916859939210b7
joechaeik <joe.chaeik@gmail.com>
2026-03-12 13:41:07 +0900

    Update member: Wonyong Choi

--- data/members.js
+++ data/members.js
@@ -2,15 +2,19 @@ var MEMBERS = {
   "professor": [
     {
       "name": "Wonyong Choi",
+      "photo": "images/members/wychoi.jpg",
       "title": "Professor",
       "email": "wchoi@kentech.ac.kr",
-      "photo": "images/members/wychoi.jpg",
       "affiliation": "Department of Energy Engineering, KENTECH",
       "bio": "Prof. Wonyong Choi leads the Eco-friendly Photoenergy Application (EPA) Laboratory at KENTECH. His research focuses on semiconductor-based photo(electro)chemical systems for environmental and energy applications. He is a Highly Cited Researcher (Clarivate) and a member of the National Academy of Engineering (NAE).",
       "links": {
         "google_scholar": "https://scholar.google.com/citations?user=BvtyVgIAAAAJ&hl=ko",
         "researchgate": "",
         "website": ""
+      },
+      "cv": {
+        "en": "https://raw.githubusercontent.com/joechaeik/epa-homepage/main/files/cv/cv_en_1773290463535_CV(Choi)_KENTECH_250321.pdf",
+        "ko": "https://raw.githubusercontent.com/joechaeik/epa-homepage/main/files/cv/cv_ko_1773290465871_최원용_이력서_250321.pdf"
       }
     }
   ],
