7216b48e6fc84da155b249797dc07135a14e2ac8
joechaeik <joe.chaeik@gmail.com>
2026-03-12 15:23:28 +0900

    Update news: 도레이과학진흥재단 과학기술상에 최원용·이필호 교수

--- data/news.js
+++ data/news.js
@@ -59,7 +59,7 @@ var NEWS = [
     "date": "2024-09-04",
     "title": "도레이과학진흥재단 과학기술상에 최원용·이필호 교수",
     "category": "Honor",
-    "pinned": false,
+    "pinned": true,
     "link": "#"
   },
   {
