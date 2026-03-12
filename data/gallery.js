e7fb13d7340d272f617f7ee0022a3ca64e870ba7
joechaeik <joe.chaeik@gmail.com>
2026-03-12 10:22:11 +0900

    Reorder gallery images

--- data/gallery.js
+++ data/gallery.js
@@ -6,14 +6,14 @@ var GALLERY = [
     "year": 2025
   },
   {
-    "src": "https://raw.githubusercontent.com/joechaeik/epa-homepage/main/images/gallery/1773275067275_240430_group_photo.jpg",
-    "caption": "240430 group photo",
+    "src": "https://raw.githubusercontent.com/joechaeik/epa-homepage/main/images/gallery/1773274352755_240207_group_photo.jpg",
+    "caption": "240207 group photo",
     "category": "Group",
     "year": 2024
   },
   {
-    "src": "https://raw.githubusercontent.com/joechaeik/epa-homepage/main/images/gallery/1773274352755_240207_group_photo.jpg",
-    "caption": "240207 group photo",
+    "src": "https://raw.githubusercontent.com/joechaeik/epa-homepage/main/images/gallery/1773275067275_240430_group_photo.jpg",
+    "caption": "240430 group photo",
     "category": "Group",
     "year": 2024
   }
